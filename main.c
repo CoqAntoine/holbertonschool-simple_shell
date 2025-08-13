979fe8f3a4f6e0f1e12dfd6b761deceedf3ae628
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 <11534@holbertonstudents.com>
2025-08-13 15:45:20 +0200

    fix reading of more than 1 argument

--- main.c
+++ main.c
@@ -5,43 +5,54 @@
 #include <sys/wait.h>
 #include <sys/types.h>
 
-extern char **environ;
+/**
+ * main - main function that open a shell and permit few commands
+ * @argc: none
+ * @argv: none
+ * @envp: array with all environnement variables
+ * Return: 0 (Succes)
+ */
 
-int main(void)
+int main(int argc, char *argv[], char **envp)
 {
-	char *line = NULL;
-	int result = 1;
+	char *line = NULL, *command[10];
 	size_t buffer = 0;
-	char *command[2];
 	pid_t child_pid;
-	int status;
+	int result = 0, status, i = 1;
 
 	while (1)
 	{
 		printf("$ ");
+		/*l'utilisateur rentre une commande*/
 		result = getline(&line, &buffer, stdin);
 		if (result == -1)
 		{
 			free(line);
 			exit(EXIT_SUCCESS);
 		}
+		/*supprime le retour à la ligne à la fin de la commande*/
 		if (line[result - 1] == '\n')
 			line[result - 1] = '\0';
-
-		command[0] = line;
-		command[1] = NULL;
-
+		/*récupère les arguments de la commande et les rentre dans un tableau*/
+		command[0] = strtok(line, " ");
+		do {
+			command[i] = strtok(NULL, " ");
+			i++;
+		} while (command[i] != NULL);
+		command[i] = NULL;
+		/*créé un processus enfant pour éxecuter la commande à l'aide d'execve*/
 		child_pid = fork();
 		if (child_pid == -1)
-			printf("ERROR\n");
+			perror("fork");
 		if (child_pid == 0)
 		{
-			if ((execve(line, command, environ)) == -1)
-				perror("execve");
+			if ((execve(command[0], command, envp)) == -1)
+				perror("execve"), exit(EXIT_FAILURE);
 		}
 		else
 			wait(&status);
 	}
+	/*retour de fin*/
 	free(line);
 	return (0);
 }
