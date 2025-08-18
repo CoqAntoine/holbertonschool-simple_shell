67362b39e18c3619cfe1ec1a57076122f067e728
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 <11534@holbertonstudents.com>
2025-08-18 10:31:30 +0200

    fix so user can write ls instead of /bin/ls

--- main.c
+++ main.c
@@ -22,23 +22,30 @@ void handle_sigint(int sig)
 
 int main(int argc, char *argv[], char **envp)
 {
-	char *line = NULL, *command[10], error_string[256];
+	char *line = NULL, *command[10], error_string[256], *folder = NULL, *command_path = NULL, *bl = NULL;
+	char *our_path = getenv("PATH");
 	size_t buffer = 0;
 	pid_t child_pid;
-	int result = 0, count_shell = 0, status, i = 1;
+	int result = 0, count_shell = 0, status, i = 0;
 
 	/*prise en compte du ctrl+C lors de la saisie*/
 	signal(SIGINT, handle_sigint);
+
 	/*evite le warning (non utilisation de argc)*/
 	if (i == 2)
 		printf("%d", argc);
+
+	printf("folder : %s, command_path : %s, our_path : %s\n", folder, command_path, our_path);
 	while (1)
 	{
+		/*prompt*/
 		if (isatty(STDIN_FILENO))
 			printf("$ ");
 		count_shell++;
+
 		/*l'utilisateur rentre une commande*/
 		result = getline(&line, &buffer, stdin);
+
 		/*prise en compte du ctrl+D lors de la saisie*/
 		if (result == -1)
 		{
@@ -47,15 +54,37 @@ int main(int argc, char *argv[], char **envp)
 				printf("\n");
 			exit(EXIT_SUCCESS);
 		}
+
 		/*supprime le retour à la ligne à la fin de la commande*/
 		if (line[result - 1] == '\n')
 			line[result - 1] = '\0';
+
+		/*vérifie si la ligne de commande existe dans le path*/
+		bl = malloc(strlen(our_path) + 1);
+		bl = strtok(line, " ");
+		folder = malloc(strlen(our_path) + 1);
+		command_path = malloc(strlen(our_path) + 1);
+		folder = strtok(our_path, ":");
+		while (folder != NULL)
+		{
+			sprintf(command_path, "%s/%s", folder, bl);
+			if (access(command_path, X_OK) == 0)
+			{
+				break;
+			}
+			else
+			{
+				folder = strtok(NULL, ":");
+			}
+		}
+			
 		/*récupère les arguments de la commande et les rentre dans un tableau*/
 		i = 1;
 		command[0] = strtok(line, " ");
 		while ((command[i] = strtok(NULL, " ")) != NULL)
 			i++;
 		command[i] = NULL;
+
 		/*créé un processus enfant pour éxecuter la commande à l'aide d'execve*/
 		if (command[0] == NULL)
     		continue;
@@ -64,7 +93,7 @@ int main(int argc, char *argv[], char **envp)
 			perror("./shell");
 		if (child_pid == 0)
 		{
-			if ((execve(command[0], command, envp)) == -1)
+			if ((execve(command_path, command, envp)) == -1)
 			{
 				sprintf(error_string, "%s: %i: %s", argv[0], count_shell, command[0]);
 				perror(error_string);
